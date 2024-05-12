683870436973f64613df931e1a76585bf707cde9
jabiryoosuf <jabiribnuyoosuf@gmail.com>
2024-05-12 22:49:29 +0530

    added controller

---
+++ controllers/movies.controller.js
@@ -0,0 +1,12 @@
+export const CreateMovie = (req, res) => {
+  res.send("create a movie");
+};
+export const GetAllMovie = (req, res) => {
+  res.send("Reading a movie");
+};
+export const UpdateMovie = (req, res) => {
+  res.send("update a movie");
+};
+export const DelateMovie = (req, res) => {
+  res.send("deleat a movie");
+};
--- routes/movies.js
+++ routes/movies.js
@@ -1,26 +1,18 @@
 import express from "express";
+import { CreateMovie, DelateMovie, GetAllMovie, UpdateMovie } from "../controllers/movies.controller.js";
 
 const router = express.Router();
 
 // C - for Creatinging
-router.post("/", (req,res) => {
-    res.send("create a movie")
-});
+router.post("/", CreateMovie);
 
 // R - for Reading
-router.get("/", (req,res) => {
-    res.send("Reading a movie")
-});
+router.get("/", GetAllMovie);
 
 // U - for Updating
-router.put("/:id", (req,res) => {
-    res.send("update a movie")
-});
+router.put("/:id",UpdateMovie);
 
 // D - for Deleating
-router.delete("/:id", (req,res) => {
-    res.send("deleat a movie")
-});
-
+router.delete("/:id", DelateMovie);
 
 export default router;
